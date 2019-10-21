17c575b9622eebf31c50816950c4614e3aba329d
Fern <pitiporn.panpoonsup19@imperial.ac.uk>
2019-10-21 01:15:52 +0100

    Python basics

---
+++ Week-2/Python basics.py
@@ -0,0 +1,28 @@
+10+5
+3-99
+5.5*3.2
+a=10+5
+b=3-99
+c=5.5*3.2
+a
+print(b)
+type(100+4)
+type(3.0+0.6)
+number = 47
+print(number)
+apples = 3
+oranges = 4
+fruit = apples + oranges
+print(fruit)
+long_string= """
+This is my very long string.
+It can even contain line breaks.
+"""
+4>6
+5==5
+10<=20
+import math
+print(math.e)
+radius = 5
+area = radius**2*math.pi
+print(area)
